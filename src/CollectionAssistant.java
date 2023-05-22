e57568572b76054a9d90d7b31147a7ed16ed9a40
FacelessRobotMan <121995215+FacelessRobotMan@users.noreply.github.com>
2023-05-21 23:20:41 -0500

    small amount of progress

--- src/CollectionAssistant.java
+++ src/CollectionAssistant.java
@@ -1,2 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+
+
+
+
 public class CollectionAssistant {
+    String username;
+    HashMap<String, String> wishlist;
+    HashMap<String, String> myCollection;
+    Scanner scanner;
+
+    public CollectionAssistant() { //initialize a new instance of public class CollectionAssistant
+        username = "guest";
+        wishlist = new HashMap<>();
+        myCollection = new HashMap<>();
+        scanner = new Scanner(in);
+    }
+
+
+
 }
